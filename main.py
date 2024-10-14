2a18c4450d89f8540de0c0ef7158a12970d19f09
rboyd1517 <56694682+rboyd1517@users.noreply.github.com>
2024-10-14 13:27:51 -0500

    Update hostname due to Porkbun changes (untested)

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ name = config['name']
 resp = urllib.request.urlopen('http://wtfismyip.com/text')
 ip = resp.read().decode('utf-8').strip('\n')
 
-resp = urllib.request.urlopen(f'https://porkbun.com/api/json/v3/dns/retrieve/{domain}/{id}',
+resp = urllib.request.urlopen(f'https://api.porkbun.com/api/json/v3/dns/retrieve/{domain}/{id}',
                               json.dumps(keys).encode('utf-8'))
 data = json.loads(resp.read().decode('utf-8'))
 
@@ -23,6 +23,6 @@ old_ip = data['records'][0]['content']
 if old_ip != ip:
     print(f'Updating ip from {old_ip} to {ip}.')
     post = dict(**keys, type='A', content=ip, name=name)
-    resp = urllib.request.urlopen(f'https://porkbun.com/api/json/v3/dns/edit/{domain}/{id}',
+    resp = urllib.request.urlopen(f'https://api.porkbun.com/api/json/v3/dns/edit/{domain}/{id}',
                                   json.dumps(post).encode('utf-8'))
 
